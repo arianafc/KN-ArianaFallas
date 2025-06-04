865353fe3d4287c2728b121beeafc47775133765
arianafc <arianafc1108@gmail.com>
2025-06-03 21:38:28 -0600

    Semana 4

--- KProyecto/Controllers/HomeController.cs
+++ KProyecto/Controllers/HomeController.cs
@@ -3,23 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using KProyecto.Models;
 
 namespace KProyecto.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+
+        //todas las acciones van en pares, es decir, dos index usualmente. 1 es un http get y el otro http post
+
+        [HttpGet] //A nivel de web, se usan para abrir las vistas
+        public ActionResult Index() 
         {
+            //var autenticacion = new Autenticacion();
+            //autenticacion.NombreUsuario = "Hola";
+
+            // return View(autenticacion);
+
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost] //Ejecutan acciones de las vistas con botones de tipo submit
+        public ActionResult Index(Autenticacion autenticacion)
         {
-            ViewBag.Message = "Your application description page.";
+
+            ViewBag.Mensaje = "No se pudo validar su información"; //mandar algo a la vista sin que forme parte del controlador
 
             return View();
+
+            //return RedirectToAction("Principal", "Home"); //redireccionamiento
         }
 
+        [HttpGet]
+
+        public ActionResult Registro()
+        {
+            return View();
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        [HttpGet]
         public ActionResult Principal()
         {
             return View();
---
+++ KProyecto/Models/Autenticacion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KProyecto.Models
+{
+    public class Autenticacion
+    {
+        public string NombreUsuario { get; set; }
+
+        public string Contrasenna { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Correo { get; set; }
+    }
+}
